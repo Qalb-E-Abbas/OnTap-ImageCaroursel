94408cee2b07f8d1bb8bad6ae73dcb983915cc95
Qalb E Abbas <qalbeabbas964@gmail.com>
2021-01-25 22:02:12 +0500

    This is initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -18,8 +18,8 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int photoIndex = 0;
 
+  int photoIndex = 0;
   List<String> photos = [
     'assets/burger1.jpg',
     'assets/burger2.jpg',
@@ -42,59 +42,244 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
-        appBar: new AppBar(
-          title: new Text('Carousel'),
-          centerTitle: true,
-        ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: Stack(
+      body: ListView(
+        shrinkWrap: true,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Stack(
                 children: <Widget>[
                   Container(
+                    height: 210.0,
                     decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(25.0),
                         image: DecorationImage(
                             image: AssetImage(photos[photoIndex]),
                             fit: BoxFit.cover)),
-                    height: 400.0,
-                    width: 300.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: <Widget>[
+                      IconButton(
+                        icon: Icon(Icons.arrow_back_ios),
+                        color: Colors.white,
+                        onPressed: () {},
+                      ),
+                      IconButton(
+                        icon: Icon(Icons.favorite),
+                        color: Colors.pink,
+                        onPressed: () {},
+                      )
+                    ],
+                  ),
+                  GestureDetector(
+                    child: Container(
+                      height: 210.0,
+                      width: MediaQuery.of(context).size.width,
+                      color: Colors.transparent,
+                    ),
+                    onTap: _nextImage,
+                  ),
+                  GestureDetector(
+                    child: Container(
+                      height: 210.0,
+                      width: MediaQuery.of(context).size.width / 2,
+                      color: Colors.transparent,
+                    ),
+                    onTap: _previousImage,
                   ),
                   Positioned(
-                    top: 375.0,
-                    left: 25.0,
-                    right: 25.0,
-                    child: SelectedPhoto(numberOfDots: photos.length, photoIndex: photoIndex),
+                    top: 180.0,
+                    left: 5.0,
+                    child: Row(
+                      children: <Widget>[
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.amber,
+                        ),
+                        Icon(
+                          Icons.star,
+                          color: Colors.grey,
+                        ),
+                        SizedBox(width: 2.0),
+                        Text(
+                          '4.0',
+                          style: TextStyle(
+                              color: Colors.white, fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(width: 4.0),
+                        SelectedPhoto(
+                            photoIndex: photoIndex, numberOfDots: photos.length)
+                      ],
+                    ),
                   )
                 ],
               ),
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                RaisedButton(
-                  child: Text('Next'),
-                  onPressed: _nextImage,
-                  elevation: 5.0,
-                  color: Colors.green,
+              SizedBox(height: 15.0),
+              Container(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      'OPEN NOW UNTIL 7PM',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 15.0),
+                    Text(
+                      'The Cinnamon Snail',
+                      style: TextStyle(
+                          color: Colors.black,
+                          fontSize: 27.0,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold),
+                    ),
+                    SizedBox(height: 10.0),
+                    Row(
+                      children: <Widget>[
+                        Text(
+                          '17th st & Union Sq East',
+                          style: TextStyle(
+                              color: Colors.grey,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold),
+                        ),
+                        SizedBox(width: 5.0),
+                        Icon(Icons.location_on),
+                        SizedBox(width: 5.0),
+                        Text(
+                          '400ft Away',
+                          style: TextStyle(
+                              color: Colors.black.withOpacity(0.5),
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    ),
+                    SizedBox(height: 7.0),
+                    Row(
+                      children: <Widget>[
+                        Icon(Icons.wifi, color: Colors.green),
+                        SizedBox(width: 4.0),
+                        Text(
+                          'Location confirmed by 3 users today',
+                          style: TextStyle(
+                              color: Colors.green,
+                              fontFamily: 'Montserrat',
+                              fontWeight: FontWeight.bold),
+                        )
+                      ],
+                    )
+                  ],
                 ),
-                SizedBox(width: 10.0),
-                RaisedButton(
-                  child: Text('Prev'),
-                  onPressed: _previousImage,
-                  elevation: 5.0,
-                  color: Colors.blue,
-                )
-              ],
+              ),
+              SizedBox(height: 20.0),
+              Container(
+                padding: EdgeInsets.only(left: 20.0),
+                child: Row(
+                  children: <Widget>[
+                    Text(
+                      'FEATURED ITEMS',
+                      style: TextStyle(
+                          color: Colors.grey,
+                          fontFamily: 'Montserrat',
+                          fontWeight: FontWeight.bold),
+                    )
+                  ],
+                ),
+              ),
+              SizedBox(height: 10.0),
+              _buildListItem(photos[0]),
+              SizedBox(height: 10.0),
+              _buildListItem(photos[1]),
+              SizedBox(height: 10.0),
+              _buildListItem(photos[2]),
+              SizedBox(height: 10.0),
+              _buildListItem(photos[3]),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget _buildListItem(String picture) {
+    return Column(
+      children: <Widget>[
+        Row(
+          children: <Widget>[
+            Container(
+              height: 100.0,
+              width: 100.0,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image: AssetImage(picture),
+                      fit: BoxFit.cover
+                  ),
+                  borderRadius: BorderRadius.only(topRight: Radius.circular(10.0), bottomRight: Radius.circular(10.0))
+              ),
+            ),
+            SizedBox(width: 10.0),
+            Container(
+              height: 100.0,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text('Maple Mustard Tempeh',
+                    style: TextStyle(
+                        fontSize: 15.0,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold
+                    ),
+                  ),
+                  SizedBox(height: 5.0),
+                  Text('Marinated kale, onion, tomato and roasted',
+                    style: TextStyle(
+                        fontSize: 11.0,
+                        fontFamily: 'Montserrat',
+                        // fontWeight: FontWeight.bold,
+                        color: Colors.grey
+                    ),
+                  ),
+                  Text('garlic aioli on grilled spelt bread',
+                    style: TextStyle(
+                        fontSize: 11.0,
+                        fontFamily: 'Montserrat',
+                        // fontWeight: FontWeight.bold,
+                        color: Colors.grey
+                    ),),
+                  SizedBox(height: 10.0),
+                  Text('\$11.25',
+                    style: TextStyle(
+                        fontSize: 20.0,
+                        fontFamily: 'Montserrat',
+                        fontWeight: FontWeight.bold,
+                        color: Colors.grey
+                    ),)
+                ],
+              ),
             )
           ],
-        ));
+        )
+      ],
+    );
   }
 }
 
 class SelectedPhoto extends StatelessWidget {
-
   final int numberOfDots;
   final int photoIndex;
 
@@ -108,12 +293,9 @@ class SelectedPhoto extends StatelessWidget {
             height: 8.0,
             width: 8.0,
             decoration: BoxDecoration(
-                color: Colors.grey,
-                borderRadius: BorderRadius.circular(4.0)
-            ),
+                color: Colors.grey, borderRadius: BorderRadius.circular(4.0)),
           ),
-        )
-    );
+        ));
   }
 
   Widget _activePhoto() {
@@ -128,12 +310,8 @@ class SelectedPhoto extends StatelessWidget {
               borderRadius: BorderRadius.circular(5.0),
               boxShadow: [
                 BoxShadow(
-                    color: Colors.grey,
-                    spreadRadius: 0.0,
-                    blurRadius: 2.0
-                )
-              ]
-          ),
+                    color: Colors.grey, spreadRadius: 0.0, blurRadius: 2.0)
+              ]),
         ),
       ),
     );
@@ -142,15 +320,12 @@ class SelectedPhoto extends StatelessWidget {
   List<Widget> _buildDots() {
     List<Widget> dots = [];
 
-    for(int i = 0; i< numberOfDots; ++i) {
-      dots.add(
-          i == photoIndex ? _activePhoto(): _inactivePhoto()
-      );
+    for (int i = 0; i < numberOfDots; ++i) {
+      dots.add(i == photoIndex ? _activePhoto() : _inactivePhoto());
     }
 
     return dots;
   }
-
 
   @override
   Widget build(BuildContext context) {
